8093695e50f0c4231b1787678bc89f0b189f272d
DangeL <92173667+DangeL187@users.noreply.github.com>
2023-09-13 11:40:31 +0000

    Add files via upload

---
+++ Client/Client.cpp
@@ -0,0 +1,55 @@
+#include "Client.h"
+#include <memory>
+
+using namespace std;
+
+class ClientWrapper {
+private:
+    shared_ptr<Client> client;
+    Message old_output;
+public:
+    ClientWrapper(std::string login, std::string ip, int port) {
+        client = make_shared<Client>(login, ip, port);
+        thread thread(&getMessage, this);
+        thread.detach();
+
+        sendMessage();
+    }
+    void sendMessage() {
+        std::string input;
+        while (true) {
+            cin >> input;
+            if (input == "exit") {
+                break;
+            }
+            else if (input == "/join") {
+            	cout << "Room: ";
+                cin >> input;
+                client->sendMessage(input, 1);
+            }
+            else if (input != "" && input != "\n") {
+                client->sendMessage(input);
+            }
+        }
+    }
+    void getMessage() {
+        while (true) {
+            Message output = client->getOutput();
+            if (!(output == old_output)) {
+                cout << output.sender << ": " << output.msg << endl;
+            }
+            old_output = output;
+        }
+    }
+};
+
+int main() {
+    string input;
+
+    cout << "Logi2n: ";
+    cin >> input;
+
+    ClientWrapper client_wrapper(input, "127.0.0.1", 1234);
+
+    return 0;
+}
